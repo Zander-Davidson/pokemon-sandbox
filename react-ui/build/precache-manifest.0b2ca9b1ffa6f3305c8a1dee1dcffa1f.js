62df93d549041ed7aacf58c7cd91b7e04b696da1
Zander-Davidson <alexander.davidson98@gmail.com>
2020-08-30 16:38:17 -0500

    react app 4

---
+++ react-ui/build/precache-manifest.0b2ca9b1ffa6f3305c8a1dee1dcffa1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd37abc5353b53b719f1",
+    "url": "/static/css/main.ab253d7e.chunk.css"
+  },
+  {
+    "revision": "bd37abc5353b53b719f1",
+    "url": "/static/js/main.a9c3e3c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2129598c429835081c9c",
+    "url": "/static/css/2.3806bc60.chunk.css"
+  },
+  {
+    "revision": "2129598c429835081c9c",
+    "url": "/static/js/2.89265e94.chunk.js"
+  },
+  {
+    "revision": "0d8a58a904412c3b81efbe21e00f5770",
+    "url": "/static/media/sandygast.0d8a58a9.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5056ab6ba3b73a782d44317d95343b2d",
+    "url": "/index.html"
+  }
+];
